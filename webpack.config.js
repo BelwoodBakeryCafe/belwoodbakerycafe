1773961cfeb5530d5ea7301b1d7a0d8d2ae15699
Benjamin Tran <benjamin.tran25@gmail.com>
2017-03-07 15:30:53 -0800

    [ DEV ] add webpack devserver

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,9 @@ module.exports = {
     }),
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor', 'manifest']
+    }), 
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ],
   externals: {
