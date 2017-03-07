568d306bd62ec7d47bd2c09276c103aa31ccf851
Benjamin Tran <benjamin.tran25@gmail.com>
2017-03-07 14:25:10 -0800

    [ DEV ] Add rimraf for build cleaning

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     path: PATHS.dist,
-    filename: '[name].bundle.js',
+    filename: '[name].[chunkhash].js',
   },
   module: {
     rules: [
@@ -37,6 +37,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'BelwoodBakeryCafe',
       template: PATHS.app + '/index.html'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
     })
   ],
   externals: {
