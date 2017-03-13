b18ecc848972463fccaa2b1782ef23cc983114ad
Benjamin Tran <benjamin.tran25@gmail.com>
2017-03-13 14:39:18 -0700

    [ DEV ] quick edit eslint

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
 
 class App extends Component {
   render() {
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path  = require('path');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,17 +9,17 @@ app.set('port', port);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'dist')));
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, 'dist/index.html'));
-  // });
+    app.use(express.static(path.join(__dirname, 'dist')));
+    // app.get('*', (req, res) => {
+    //   res.sendFile(path.join(__dirname, 'dist/index.html'));
+    // });
 } else {
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack.config.js');
-  app.use(webpackMiddleware(webpack(webpackConfig)));
+    const webpackMiddleware = require('webpack-dev-middleware');
+    const webpack = require('webpack');
+    const webpackConfig = require('./webpack.config.js');
+    app.use(webpackMiddleware(webpack(webpackConfig)));
 }
 
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}/`);
-});  
+    console.log(`Listening on http://localhost:${port}/`);
+});
