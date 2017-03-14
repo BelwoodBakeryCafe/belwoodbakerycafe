5cc5a1b5dae6baa9e27bc2af6e2e0dae832214bb
Benjamin Tran <benjamin.tran25@gmail.com>
2017-03-13 19:49:58 -0700

    [ FEAT ] Add in main reducers file

---
+++ client/reducers/reducers.js
@@ -0,0 +1,45 @@
+import { fromJS } from 'immutable';
+import { combineReducers } from 'redux';
+import { LOCATION_CHANGE } from 'react-router-redux';
+
+// import globalReducer from 'containers/App/reducer';
+// import languageProviderReducer from 'containers/LanguageProvider/reducer';
+
+/*
+ * routeReducer
+ *
+ * The reducer merges route location changes into our immutable state.
+ * The change is necessitated by moving to react-router-redux@4
+ *
+ */
+
+// Initial routing state
+const routeInitialState = fromJS({
+    locationBeforeTransitions: null,
+});
+
+/**
+ * Merge route into the global application state
+ */
+function routeReducer(state = routeInitialState, action) {
+    switch (action.type) {
+        /* istanbul ignore next */
+        case LOCATION_CHANGE:
+            return state.merge({
+                locationBeforeTransitions: action.payload,
+            });
+        default:
+            return state;
+    }
+}
+
+/**
+ * Creates the main reducer with the asynchronously loaded ones
+ */
+export default function createReducer(asyncReducers) {
+    return combineReducers({
+        route: routeReducer,
+        global: globalReducer,
+        ...asyncReducers,
+    });
+}
