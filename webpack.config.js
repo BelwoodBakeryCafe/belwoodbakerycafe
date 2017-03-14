8f8a40a6476090f5d25ed39df9addb0e0885e0cc
Benjamin Tran <benjamin.tran25@gmail.com>
2017-03-13 23:32:01 -0700

    [ DEV ] Change babel loader test to address both js ans jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,55 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
-  app: path.resolve(__dirname, './client'),
-  dist: path.resolve(__dirname, './dist'),
-  style: [path.resolve(__dirname, './client/styles')],
-  vendor: path.resolve(__dirname, './vendor.jsx')
+    app: path.resolve(__dirname, './client'),
+    dist: path.resolve(__dirname, './dist'),
+    style: [path.resolve(__dirname, './client/styles')],
+    vendor: path.resolve(__dirname, './vendor.jsx')
 };
 
 const VENDOR_LIBS = ['lodash', 'react', 'react-dom', 'react-redux', 'react-router', 'react-router-bootstrap', 'react-router-redux', 'redux', 'redux-form', 'redux-thunk'];
 
 module.exports = {
-  entry: { 
-    app: PATHS.app + '/app.jsx',
-    vendor: VENDOR_LIBS
-  },
-  output: {
-    path: PATHS.dist,
-    filename: '[name].[chunkhash].js',
-  },
-  module: {
-    rules: [
-      {
-        use: 'babel-loader',
-        test: /\.jsx$/,
-        exclude: /node_modules/
-      },
-      {
-        use: ['style-loader', 'css-loader'],
-        test: /\.css$/
-      },
+    entry: {
+        app: PATHS.app + '/app.jsx',
+        vendor: VENDOR_LIBS
+    },
+    output: {
+        path: PATHS.dist,
+        filename: '[name].[chunkhash].js',
+    },
+    module: {
+        rules: [{
+                use: 'babel-loader',
+                test: /\.jsx?$/,
+                include: PATHS.app,
+                exclude: /node_modules/
+            },
+            {
+                use: ['style-loader', 'css-loader'],
+                test: /\.css$/
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'BelwoodBakeryCafe',
+            template: PATHS.app + '/index.html'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'BelwoodBakeryCafe',
-      template: PATHS.app + '/index.html'
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor', 'manifest']
-    }), 
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ],
-  externals: {
-    'react/addons': true,
-    'react/lib/ExecutionEnvironment': true,
-    'react/lib/ReactContext': true
-  }
+    resolve: {
+        //Empty string needed. 
+        extensions: ['.js', '.jsx']
+    },
+    externals: {
+        'react/addons': true,
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
+    }
 };
