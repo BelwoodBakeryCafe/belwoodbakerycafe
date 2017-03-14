bac678759030cd6da94ce401128e78f8d65e769d
Benjamin Tran <benjamin.tran25@gmail.com>
2017-03-13 19:48:43 -0700

    [ FEAT|DEV ] Configure redux and router

--- client/app.jsx
+++ client/app.jsx
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory, applyRouterMiddleware } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="mainApp">
-        <div>Hello World</div>
-      </div>
-    );
-  }
-}
+// Load the favicon, the manifest.json file and the .htaccess file
+/* eslint-disable import/no-webpack-loader-syntax */
+// import '!file-loader?name=[name].[ext]!./favicon.ico';
+// import '!file-loader?name=[name].[ext]!./manifest.json';
+// import 'file-loader?name=[name].[ext]!./.htaccess'; // eslint-disable-line import/extensions
+/* eslint-enable import/no-webpack-loader-syntax */
 
-ReactDOM.render(<App />, document.getElementById('main'));
+import App from './containers/App';
+
+import configureStore from './store';
+// Import routes
+import createRoutes from './routes';
+
+const initialState = {};
+const store = configureStore(initialState, browserHistory);
+
+const history = syncHistoryWithStore(browserHistory, store, {
+  selectLocationState: makeSelectLocationState(),
+});
+const rootRoute = {
+  path: '/',
+  component: App,
+  indexRoute: { component: App },
+  childRoutes: createRoutes(store),
+};
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router
+      history={history}
+      routes={rootRoute}
+    />
+  </Provider>,
+document.getElementById('mainApp'),
+);
