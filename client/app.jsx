8b06ee156919ea0e18546bdb44d0c84c3b81571c
Benjamin Tran <benjamin.tran25@gmail.com>
2017-03-15 21:43:36 -0700

    [ FEAT ] Revert back to non code split routes

--- client/app.jsx
+++ client/app.jsx
@@ -1,52 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import Home from './containers/Home';
+import { AppContainer } from 'react-hot-loader';
+import createSelectLocationState from './selectors';
+import Root from './containers/Root';
+import configureStore from './store';
 // Load the favicon, the manifest.json file and the .htaccess file
 /* eslint-disable import/no-webpack-loader-syntax */
 // import '!file-loader?name=[name].[ext]!./favicon.ico';
 // import '!file-loader?name=[name].[ext]!./manifest.json';
 // import 'file-loader?name=[name].[ext]!./.htaccess'; // eslint-disable-line import/extensions
 /* eslint-enable import/no-webpack-loader-syntax */
-
-import configureStore from './store';
-// Import routes
-import createRoutes from './router';
-
 const initialState = {};
-const createSelectLocationState = () => {
-  let prevRoutingState, prevRoutingStateJS;
-  return (state) => {
-    const routingState = state.get('routing'); // or state.routing 
-    if (typeof prevRoutingState === 'undefined' || prevRoutingState !== routingState) {
-      prevRoutingState = routingState;
-      prevRoutingStateJS = routingState.toJS();
-    }
-    return prevRoutingStateJS;
-  };
-};
-
-const App = () => {
-  const store = configureStore(initialState, browserHistory);
-
-  const history = syncHistoryWithStore(browserHistory, store, {
+// For syncing browser history with store. Must set selectLocationState when using immutable redux
+const store = configureStore(initialState, browserHistory);
+const history = syncHistoryWithStore(browserHistory, store, {
   selectLocationState: createSelectLocationState() });
 
-  const rootRoute = {
-    component: Home,
-    path: '/',
-    childRoutes: createRoutes(store),
-  };
-  return (
-  <Provider store={store}>
-    <Router
-      history={history}
-      routes={rootRoute}
-    />
-  </Provider>
-  );
-};
+ReactDOM.render(<AppContainer><Root store={store} history={history} /> </AppContainer>, document.getElementById('main'));
 
-ReactDOM.render(<App />, document.getElementById('main'));
+if (module.hot) {
+  module.hot.accept('./containers/Home', () => {
+    ReactDOM.render(<AppContainer><Root store={store} history={history} /></AppContainer>, document.getElementById('main'));
+  });
+}
---
+++ client/selectors.js
@@ -0,0 +1,12 @@
+export default function createSelectLocationState() {
+    let prevRoutingState;
+    let prevRoutingStateJS;
+    return (state) => {
+        const routingState = state.get('routing'); // or state.routing
+        if (typeof prevRoutingState === 'undefined' || prevRoutingState !== routingState) {
+            prevRoutingState = routingState;
+            prevRoutingStateJS = routingState.toJS();
+        }
+        return prevRoutingStateJS;
+    };
+}
